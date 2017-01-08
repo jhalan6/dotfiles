b9cfe73e4645e653c5582f7de927fd8a965589fd
chenshenghui <chenshenghui@baidu.com>
2017-01-08 19:23:21 +0800

    add tmux dotFile

--- useDotFiles.sh
+++ useDotFiles.sh
@@ -6,12 +6,15 @@ echo "remove old vimrc files"
 rm -rf ${IN_USE_DIR}
 rm ~/.vimrc
 rm ~/.vimrc.bundles
+rm ~/.tmux.conf
 
 echo "create IN_USE_DIR"
 mkdir ${IN_USE_DIR}
 
 cp vimrc ./${IN_USE_DIR}/.vimrc
 cp vimrc.bundles ./${IN_USE_DIR}/.vimrc.bundles
+cp tmux.conf ./${IN_USE_DIR}/.tmux.conf
 echo "make soft link for vim"
 ln -s ${DIR}/${IN_USE_DIR}/.vimrc ~/.vimrc
 ln -s ${DIR}/${IN_USE_DIR}/.vimrc.bundles ~/.vimrc.bundles
+ln -s ${DIR}/${IN_USE_DIR}/.tmux.conf ~/.tmux.conf
